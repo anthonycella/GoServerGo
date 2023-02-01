2385ce375a817d167d449948bc75e83914ba85b6
Anthony Cella <anthony.cella242@gmail.com>
2023-02-01 15:49:26 -0500

    Deleted all of the tutorial code (commented or otherwise). Moved that code into a separate repository. Changed the name of sandbox.go to main.go (main.go will be the main file that the applciation is run on). Also wrote a second version of the factorial function recursively (called factorialRecursive) to demonstrate that I can write recursive functions.

--- go-server/main.go
+++ go-server/main.go
@@ -19,11 +19,14 @@ func factorial(inputNumber int) int {
 	return factorialResult
 }
 
+func factorialRecursive(inputNumber int) int {
+	if inputNumber < 2 {
+		return 1
+	}
+
+	return inputNumber * factorialRecursive(inputNumber-1)
+}
+
 func main() {
-	// fmt.Println("I am Batman")
-	// fmt.Println("I am going to kill you", time.Now())
-	// fmt.Println("My favorite number is", rand.Intn(10))
-	// fmt.Printf("Now you have %g problems. \n", math.Sqrt(4))
-	// fmt.Println(math.Pi)
 	fmt.Println(factorial(5))
 }
