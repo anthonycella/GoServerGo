12879a6501ac67152b7031de14d65a4c221def50
Anthony Cella <anthony.cella242@gmail.com>
2023-02-01 14:50:44 -0500

    Created the factorial function for the server to use. It is currently written and used in sandbox.go which I am using going through Go's tour to get used to it. sandbox.go has a number of commented out tutorial lines

--- go-server/sandbox.go
+++ go-server/sandbox.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
+func factorial(inputNumber int) int {
+	if inputNumber < 2 {
+		return 1
+	}
+
+	factorialResult := 1
+
+	for inputNumber > 0 {
+		factorialResult *= inputNumber
+		inputNumber -= 1
+	}
+
+	return factorialResult
+}
+
 func main() {
-	fmt.Println("I am Batman")
-	fmt.Println("I am going to kill you", time.Now())
+	// fmt.Println("I am Batman")
+	// fmt.Println("I am going to kill you", time.Now())
+	// fmt.Println("My favorite number is", rand.Intn(10))
+	// fmt.Printf("Now you have %g problems. \n", math.Sqrt(4))
+	// fmt.Println(math.Pi)
+	fmt.Println(factorial(5))
 }
